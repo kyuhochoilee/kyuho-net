5073b1b0115dae94bd8ff3a51a2671604dd1c1aa
Kyuho Lee <kyuhochoilee@gmail.com>
2023-08-07 18:17:46 -0400

    started some style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "../styles/Home.module.css"; // Import the CSS Module
 
 const Home = () => {
-  return <h1>Welcome to the Home page!</h1>;
+  return (
+    <div>
+      <div className={styles.header}>KYUHO LEE</div>
+      <div className={styles.subHeader}>
+        creative digital designer based in boston
+      </div>
+      <div className={styles.subHeader}>
+        junior @ upenn studying design / psych / computer science
+      </div>
+      <div className={styles.button}>LET'S CONNECT</div>
+    </div>
+  );
 };
 
 export default Home;
